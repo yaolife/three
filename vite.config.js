912051be233e4e5e25364e373e5ab0b8fe52757b
yaolife <yaolife@126.com>
2025-10-29 15:16:48 +0800

    数据管理

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
