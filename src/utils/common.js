6c262f8e7f25c7007dabf059b12e4f7210c3a1d6
yaolife <yaolife@126.com>
2025-11-11 11:24:55 +0800

    总平规划

--- src/utils/common.js
+++ src/utils/common.js
@@ -55,7 +55,7 @@ export const craneType=0;//三一重工550型起重机
 // 起重机吊臂类型
 export const getBoomType= () => {
   return [
-    { label: '主壁', value: 0 },
+    { label: '主臂', value: 0 },
     { label: '主臂+副臂(46+16)', value: 1 },
     { label: '主臂+副臂(46+9.2)', value: 2 },
   ];
