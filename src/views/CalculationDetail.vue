44148c6f75c2cf34503984be4f4a97b3f2ed0218
yaolife <yaolife@126.com>
2025-12-05 10:41:56 +0800

    总平

--- src/views/CalculationDetail.vue
+++ src/views/CalculationDetail.vue
@@ -1954,7 +1954,7 @@
                 {{
                   sling.loadType === 1 ? "出厂安全系数" : "额定载荷"
                 }}：{{ sling.loadType === 1 ? sling.safetyFactor : sling.ratedLoad }}
-                {{ sling.loadType === 0 ? "MPa" : "" }}
+                {{ sling.loadType === 0 ? "t" : "" }}
               </div>
             </div>
           </div>
@@ -2277,7 +2277,7 @@
                 {{
                   sling.loadType === 1 ? "出厂安全系数" : "额定载荷"
                 }}：{{ sling.loadType === 1 ? sling.safetyFactor : sling.ratedLoad }}
-                {{ sling.loadType === 0 ? "MPa" : "" }}
+                {{ sling.loadType === 0 ? "t" : "" }}
               </div>
             </div>
           </div>
