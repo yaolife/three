b2e9ea25049afb91f7f603006d216e6e46e881c7
yaolife <yaolife@126.com>
2026-01-09 17:18:52 +0800

    修改样式

--- src/views/SitePlan.vue
+++ src/views/SitePlan.vue
@@ -7657,7 +7657,7 @@ const handleBack = () => {
   display: flex;
   justify-content: space-around;
   align-items: center;
-  padding: 6px 16px;
+  padding: 3px 16px;
 }
 .punctuation{
   color: #FFF;
